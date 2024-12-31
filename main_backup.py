09f9c5986ec9059682384843711866012975cd06
TianziLuo <tluo023@ucr.edu>
2024-12-30 19:01:15 -0800

    merge the unzip func

--- main_backup.py
+++ main_backup.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from copy_configs import copy_configs
 from db_update import db_update
 from export_mysql import export_mysql
+from unzip import unzip
 
 def safe_execute(func):
     def wrapper():
@@ -14,7 +15,7 @@ def safe_execute(func):
 def main():
     root = tk.Tk()
     root.title("BackUp Panel")
-    root.geometry("400x180")  # 窗口大小
+    root.geometry("400x220")  # 窗口大小
     root.configure(bg="#0F172A")  # 窗口背景颜色
 
     # 美化按钮样式
@@ -32,13 +33,14 @@ def main():
         return tk.Button(root, text=text, command=safe_execute(command), **button_style)
 
     # 按钮组件
+    unzip_button = create_button("unzip", unzip)
     copy_configs_button = create_button("Backup Configs", copy_configs)
     export_mysql_button = create_button("Backup Database", export_mysql)
     db_update_button = create_button("db_update", db_update)
    
-    copy_configs_button.pack(pady=(20, 10))
+    unzip_button.pack(pady=(20, 10))
     # 布局（左右留白和上下间距）
-    for button in [export_mysql_button, db_update_button]:
+    for button in [copy_configs_button, export_mysql_button, db_update_button]:
         button.pack(pady=8, padx=50)  # 上下间距为10，左右留白50
 
     # 启动主窗口
